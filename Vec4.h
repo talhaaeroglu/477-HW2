e747957918804b75745da00f42a6d90f73f0fc04
ilgazsenyuz <ilgaz@jotform.com>
2022-12-14 18:36:59 +0300

    Perspective division

--- Vec4.h
+++ Vec4.h
@@ -14,7 +14,7 @@ public:
     Vec4();
     Vec4(double x, double y, double z, double t, int colorId);
     Vec4(const Vec4 &other);
-    
+    void Vec4::perspectiveDivision(Vec4 &v);
     double getElementAt(int index);
 
     friend std::ostream& operator<<(std::ostream& os, const Vec4& v);
